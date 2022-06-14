12ac3090506b2aba81465d86eda5325d46f75de9
Mateusz Ostrowski <kieloc666@gmail.com>
2022-06-14 16:01:38 +0200

    test

--- library/include/exception/SaveFilePathNotSetException.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Mateusz Ostrowski on 14/06/2022.
-//
-
-#include "SaveFilePathNotSetException.h"
-
-const char *SaveFilePathNotSetException::what() const noexcept {
-    return "Save file path not set";
-}
